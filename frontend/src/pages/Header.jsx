4386207fdfe2533ac8df245bd80c7b63bd69edaf
Md.Abu siddik <dev.antor@jsencoder.com>
2023-10-17 21:38:44 +0600

    Navber commit
    Navber commit

--- frontend/src/pages/Header.jsx
+++ frontend/src/pages/Header.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
+import { AiOutlineMenu } from "react-icons/ai";
 
 const Header = () => {
     return (
         <div>
-            
+             <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost btn-circle">
+              <AiOutlineMenu></AiOutlineMenu>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+              <li>
+                <a>Dashboard</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="flex-none gap-2">
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">Profile</a>
+              </li>
+
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
         </div>
     );
 };
