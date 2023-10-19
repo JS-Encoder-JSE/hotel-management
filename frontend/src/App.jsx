f95ea6c4313d7f6e89617760af9816941e7ac43a
MD Tajkir Alam Rion <dev.tajkir@jsencoder.com>
2023-10-19 16:58:31 +0600

    code error solved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,19 +5,10 @@ import PrivateRoute from "./routes/PrivateRoute.jsx";
 import ManagerRoute from "./routes/ManagerRoute.jsx";
 import Root from "./Root.jsx";
 import SignIn from "./pages/SignIn.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
+import Dashboard from "./pages/Dashboard.jsx" ;
 import AddRoom from "./components/AddRoom.jsx";
 import AllInventory from "./components/Inventory/AllInventory.jsx";
 import AddInventory from "./components/Inventory/AddInventory.jsx";
-import Header from "./components/Header.jsx";
-import EditRoom from "./pages/room/EditRoom.jsx";
-import ManageRoom from "./pages/room/ManageRoom.jsx";
-import AddRoom from "./pages/room/AddRoom.jsx";
-import AddFood from "./components/AddFood.jsx";
-import FoodCard from "./components/Food/FoodCard.jsx";
-import EditFood from "./components/Food/EditFood.jsx";
-import InventoryFood from "./components/Food/InventoryFood.jsx";
-import ManageSingleRoom from "./pages/room/ManageSingleRoom.jsx";
 
 const App = () => {
   const router = createBrowserRouter([
@@ -50,10 +41,6 @@ const App = () => {
               element: (
                 <ManagerRoute>
                   <AllInventory />
-              path: "edit-room/:id",
-              element: (
-                <ManagerRoute>
-                  <EditRoom />
                 </ManagerRoute>
               ),
             },
@@ -62,58 +49,6 @@ const App = () => {
               element: (
                 <ManagerRoute>
                   <AddInventory />
-              path: "manage-room",
-              element: (
-                <ManagerRoute>
-                  <ManageRoom />
-                </ManagerRoute>
-              ),
-            },
-            {
-              path: "add-food",
-              element: (
-                <ManagerRoute>
-                  <AddFood />
-                </ManagerRoute>
-              ),
-            },
-            {
-              path: "Foodcard",
-              element: (
-                <ManagerRoute>
-                  <FoodCard />
-                </ManagerRoute>
-              ),
-            },
-            {
-              path: "inventoryFood",
-              element: (
-                <ManagerRoute>
-                  <InventoryFood />
-                </ManagerRoute>
-              ),
-            },
-            {
-              path: "edit-food",
-              element: (
-                <ManagerRoute>
-                  <EditFood />
-                </ManagerRoute>
-              ),
-            },
-            // {
-            //   path: "add-food",
-            //   element: <AddFood />,
-            // },
-            {
-              path: "header",
-              element: <Header />,
-            },
-            {
-              path: "manage-room/:id",
-              element: (
-                <ManagerRoute>
-                  <ManageSingleRoom />
                 </ManagerRoute>
               ),
             },
