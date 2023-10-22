a45bd9e2fe94f2fdedb0423ba58a5dc96515df4d
MD Tajkir Alam Rion <dev.tajkir@jsencoder.com>
2023-10-22 14:14:03 +0600

    checkout page implemented

---
+++ frontend/src/pages/room/CheckOut.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CheckOut = () => {
+    return (
+        <div className='max-w-4xl mx-auto'>
+            <div>
+                Room No:
+
+            </div>
+        </div>
+    );
+};
+
+export default CheckOut;
