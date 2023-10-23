1c5a64a7e1a6833071c831b887e9d5dac91b6deb
MD Tajkir Alam Rion <dev.tajkir@jsencoder.com>
2023-10-23 18:32:41 +0600

    confirm booking init

--- frontend/src/pages/restaurant/AddOrder.jsx
+++ frontend/src/pages/restaurant/AddOrder.jsx
@@ -4,6 +4,7 @@ import * as yup from "yup";
 import FoodLists from "../../components/restaurant/FoodLists.jsx";
 import { FaEye, FaFileInvoice } from "react-icons/fa";
 import Modal from "../../components/Modal.jsx";
+import ConfirmOrder from "../../components/room/ConfirmOrder.jsx";
 
 // form validation
 const validationSchema = yup.object({
@@ -65,90 +66,10 @@ const AddOrder = () => {
             Confirm Order
           </label>
           <input type="checkbox" id="my_modal_6" className="modal-toggle " />
-          {/* <div className="modal ">
-            <div className="modal-box">
-              <div className={` space-y-10 `}>
-
-                <div className={`bg-white px-10 py-5 rounded `}>
-                  <h3 className={`text-xl font-semibold`}>Order</h3>
-                  <hr className={`my-5`} />
-                  <div className={`space-y-10`}>
-                    <div className={`flex justify-between`}>
-                    </div>
-                    <div className="overflow-x-auto w-full">
-                      <table className="table">
-                        <thead>
-                          <tr>
-                            <th>SL</th>
-                            <th>item</th>
-                            <th>Price</th>
-                            <th>Quantity</th>
-                            <th>Total</th>
-                            <th>Action</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {[...Array(+formik.values.entries || 2)].map((_, idx) => {
-                            return (
-                              <tr
-                              // className={idx % 2 === 0 ? "bg-gray-100 hover" : "hover"}
-                              >
-                                <th>{++idx}</th>
-                                <td>programe</td>
-                                <td>104</td>
-                                <td>
-                                  <span className={`border p-2`}>+</span>
-                                  <span className={`border px-5 py-2`}></span>
-                                  <span className={`border p-2`}>-</span>
-
-                                </td>
-                                <td>
-                                  120
-                                </td>
-
-                                <td className={`space-x-1.5`}>
-
-                                  <span
-                                    className={`btn btn-sm bg-green-slimy hover:bg-transparent text-white hover:text-green-slimy !border-green-slimy rounded normal-case`}
-                                  >
-                                    <FaFileInvoice />
-                                  </span>
-                                </td>
-                              </tr>
-                            );
-                          })}
-                        </tbody>
-                        <tfoot className={`text-sm`}>
-                          <tr>
-                            <td colSpan={5} className={`text-end`}><div className="card  bg-white-300 shadow-xl">
-                              <div className="card-body text-center max-auto">
-                                <h2 className="card-title underline">Cart </h2>
-                                <p>Total Price : $120</p>
-                                <p>Tax : $20</p>
-                                <p>Grand Total: $140</p>
-                                <div>
-                                  <button className="btn bg-green-400">Clear cart</button>
-                                  <button className="btn bg-green-400">CheckOut</button>
-                                </div>
-                              </div>
-                            </div></td>
-                            <td></td>
-
-                          </tr>
-                        </tfoot>
-                      </table>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="modal-action">
-                <label htmlFor="my_modal_6" className="btn">Close!</label>
-              </div>
-            </div>
-          </div> */}
           <Modal id={`my_modal_6`}>
-            hello
+            <ConfirmOrder />
           </Modal>
+
           {/* modal */}
 
           <input
