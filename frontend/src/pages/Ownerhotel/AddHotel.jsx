8b57bc0dc978aa6b68c2ed0a9b4c7041661b0892
MD Tajkir Alam Rion <dev.tajkir@jsencoder.com>
2023-10-27 00:49:45 +0600

    bg change

--- frontend/src/pages/Ownerhotel/AddHotel.jsx
+++ frontend/src/pages/Ownerhotel/AddHotel.jsx
@@ -39,7 +39,7 @@ const AddHotel = () => {
         <div className="card-body p-4">
           <div className="text-2xl md:flex justify-between items-center">
             <h2>Add Hotels</h2>
-            <h2 className="shadow px-4 py-2 rounded-md text-green-slimy inline-block">
+            <h2 className="shadow-lg bg-slate-100 px-4 py-2 rounded-md text-green-slimy inline-block">
               {hotelLimit >= 1 ?
                 <>
                   You can add
