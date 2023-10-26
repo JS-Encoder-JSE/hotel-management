31db15e7fc554bcbc71fa1ec961e2903a749e37b
MD Tajkir Alam Rion <dev.tajkir@jsencoder.com>
2023-10-26 17:15:41 +0600

    financial monitor hotel search implemented

--- frontend/src/pages/MonitorFinance/MonitorFinance.jsx
+++ frontend/src/pages/MonitorFinance/MonitorFinance.jsx
@@ -1,28 +1,48 @@
 import React from 'react';
-import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
-import './monitorfinance.css'
-import UserDashBoard from '../../components/UserDashBoard/UserDashBoard';
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
+
+
 
 const MonitorFinance = () => {
+    const animatedComponents = makeAnimated();
+     
+    // This portion will come from api. and After fetching api needs a state [roomList, setRoomList]
+     const hotelList = [
+        // { value: '', label: 'Room Select' },
+        { value: 'Hotel - 1', label: '1 - Chocolate' },
+        { value: 'Hotel - 2', label: '2 - Strawberry' },
+        { value: 'Hotel - 3', label: '3 - Shake' },
+        { value: 'Hotel - 4', label: '4 - AC' },
+        { value: 'Hotel - 5', label: '5 - None AC' },
+        { value: 'Hotel - 6', label: '6 - Fan' },
+        { value: 'Hotel - 7', label: '7 - Deluxe' },
+        { value: 'Hotel - 8', label: '8 - None-Deluxe' },
+        { value: 'Hotel - 9', label: '9 - Couple' },
+        { value: 'Hotel - 10', label: '10 - Anniversary' },
+        { value: 'Hotel - 11', label: '11 - Official' },
+        { value: 'Hotel - 12', label: '12 - VIP' },
+    ]
+
+    const handleSearchHotel = e => {
+        console.log(e.value);
+    }
 
     return (
-        <div>
-            <Tabs>
-                <TabList>
-                    {
-                        [...Array(20)].map((hotel, index) =>
-                            <Tab>Hotel {++index}</Tab>
-                        )
-                    }
-                </TabList>
-                {
-                    [...Array(20)].map((hotel, index) =>
-                        <TabPanel>
-                            <UserDashBoard></UserDashBoard>
-                        </TabPanel>
-                    )
-                }
-            </Tabs>
+        <div className='space-y-8'>
+            {/* Select Room Section */}
+            <section className='max-w-3xl mx-auto flex gap-5 items-center'>
+                <p>Hotel Name :</p>
+                <div className='w-[353px]'>
+                    <Select
+                        components={animatedComponents}
+                        options={hotelList}
+                        placeholder='Search with hotel name'
+                        onChange={(e) => handleSearchHotel(e)}
+                    />
+                </div>
+            </section>
+
         </div>
     );
 };
