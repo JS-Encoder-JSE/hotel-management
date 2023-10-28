124362f7a14781874c7bc3f15d7f922d5db002e7
MD Tajkir Alam Rion <dev.tajkir@jsencoder.com>
2023-10-28 12:39:28 +0600

    profile secton added

--- frontend/src/components/room/RoomTabs.jsx
+++ frontend/src/components/room/RoomTabs.jsx
@@ -1,34 +1,23 @@
 import React from "react";
-import { Tabs, TabList, TabPanel, Tab } from "react-tabs";
+// import { Tabs, TabList, TabPanel, Tab } from "react-tabs";
 
 const RoomTabs = () => {
   return (
-    <Tabs>
-      <TabList className="tabs w-fit mb-4">
-        <Tab
-          className="tab tab-bordered"
-          selectedClassName="tab-active text-green-slimy !border-b-green-slimy"
-        >
-          <span className={`text-xl`}>Description</span>
-        </Tab>
-      </TabList>
-      <TabPanel>
+    // <Tabs>
+    //   <TabList className="tabs w-fit mb-4">
+    //     <Tab
+    //       className="tab tab-bordered"
+    //       selectedClassName="tab-active text-green-slimy !border-b-green-slimy"
+    //     >
+    //       <span className={`text-xl`}>Description</span>
+    //     </Tab>
+    //   </TabList>
+    //   <TabPanel>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa maxime
-          quis quod. Assumenda consequuntur cum eaque exercitationem nemo quae,
-          quaerat qui quidem. Commodi consectetur eos maiores nemo odit quia
-          recusandae reprehenderit suscipit veniam! Asperiores atque neque,
-          obcaecati quibusdam quidem quis sapiente soluta? Animi cum dolor
-          doloremque impedit incidunt iste minima minus modi nihil quis quod
-          saepe totam, voluptatum. Consectetur dolor facere minima natus
-          ratione? Blanditiis cum dolore ex illum inventore maiores nam
-          necessitatibus perspiciatis praesentium repellat. Animi fugiat
-          provident rem totam? Architecto esse laboriosam, laborum libero
-          officiis omnis porro quibusdam quod voluptate! Doloremque incidunt
-          labore laborum officia reiciendis? Minus, similique?
+          Lorem ipsum
         </p>
-      </TabPanel>
-    </Tabs>
+      // </TabPanel>
+    // </Tabs> 
   );
 };
 
