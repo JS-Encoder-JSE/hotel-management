a84f365d97c78f8f3464d6b989c38a2293369f48
MD Tajkir Alam Rion <dev.tajkir@jsencoder.com>
2023-10-28 21:27:08 +0600

    sub admin list added

--- frontend/src/pages/Admin/SubAdminListView.jsx
+++ frontend/src/pages/Admin/SubAdminListView.jsx
@@ -1,7 +1,22 @@
+import { useFormik } from "formik";
 import React from "react";
-import { FaArrowLeft } from "react-icons/fa";
+import { AiTwotoneDelete } from "react-icons/ai";
+import { FaArrowLeft, FaRegEdit } from "react-icons/fa";
+import { GrView } from "react-icons/gr";
+import { Link, useNavigate } from "react-router-dom";
 
 const SubAdminListView = () => {
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      entries: "",
+      search: "",
+      startDate: "",
+      endDate: "",
+    },
+  });
+
   return (
     <div>
       <div className="w-full rounded-xl bg-white shadow-xl p-5">
@@ -28,6 +43,62 @@ const SubAdminListView = () => {
             <h6> Sub Admin Joint Date :12-10-2023 </h6>
             <h6> Sub Admin Salary :12-10-2023 </h6>
             <h6>Status : Active</h6>
+          </div>
+        </div>
+      </div>
+
+      <div className={`bg-white px-10 py-5 mt-12 rounded-xl`}>
+        <h3 className={`text-xl font-semibold text-center`}>Manager List </h3>
+        <hr className={`my-5`} />
+        <div className={`space-y-10`}>
+          <div className="overflow-x-auto">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>SL</th>
+                  <th>Manager Name</th>
+                  <th>Manager Email</th>
+                  <th>Phone Number</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {[...Array(+formik.values.entries || 5)].map((_, idx) => {
+                  return (
+                    <tr
+                      key={idx}
+                      className={idx % 2 === 0 ? "bg-gray-100 hover" : "hover"}
+                    >
+                      <th>{++idx}</th>
+                      <td>Jon Doe</td>
+                      <td>jondoe@gmail.com</td>
+                      <td>+99801111</td>
+                      <td className={`space-x-1.5`}>
+                        <Link to={`/dashboard/managerList-view/${idx}`}>
+                          <span
+                            className={`btn btn-sm bg-transparent hover:bg-green-slimy text-green-slimy hover:text-white !border-green-slimy rounded normal-case md:mb-2 mb-2 ms-2`}
+                          >
+                            <GrView />
+                          </span>
+                        </Link>
+                        <Link to={`/dashboard/manager-edit/${idx}`}>
+                          <span
+                            className={`btn btn-sm bg-transparent hover:bg-green-slimy text-green-slimy hover:text-white !border-green-slimy rounded normal-case md:mb-2 mb-2 ms-2`}
+                          >
+                            <FaRegEdit />
+                          </span>
+                        </Link>
+                        <span
+                          className={`btn btn-sm bg-red-500 hover:bg-transparent text-white hover:text-red-500 !border-red-500 rounded normal-case`}
+                        >
+                          <AiTwotoneDelete />
+                        </span>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
