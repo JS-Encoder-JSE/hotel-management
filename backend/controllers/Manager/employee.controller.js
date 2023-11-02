66f3518e48b73389bdbc1b6ccd0c7bb177e85599
Dip Jit Baroi <dipjitbaroiofficial@gmail.com>
2023-11-02 20:46:01 +0600

    fixed conflictts

--- backend/controllers/Manager/employee.controller.js
+++ backend/controllers/Manager/employee.controller.js
@@ -1,108 +1,108 @@
-import Employee from "../../models/Manager/employee.model.js";
+// import Employee from "../../models/Manager/employee.model.js";
 
-// Controller to add a new employee
-export const addEmployee = async (req, res) => {
-  try {
-    const {
-      name,
-      designation,
-      shift,
-      sallary,
-      street_address,
-      state,
-      city,
-      zip,
-      images,
-    } = req.body;
+// // Controller to add a new employee
+// export const addEmployee = async (req, res) => {
+//   try {
+//     const {
+//       name,
+//       designation,
+//       shift,
+//       sallary,
+//       street_address,
+//       state,
+//       city,
+//       zip,
+//       images,
+//     } = req.body;
 
-    // Create a new employee instance based on the Employee model
-    const newEmployee = new Employee({
-      name,
-      designation,
-      shift,
-      sallary,
-      street_address,
-      state,
-      city,
-      zip,
-      images,
-    });
+//     // Create a new employee instance based on the Employee model
+//     const newEmployee = new Employee({
+//       name,
+//       designation,
+//       shift,
+//       sallary,
+//       street_address,
+//       state,
+//       city,
+//       zip,
+//       images,
+//     });
 
-    // Save the new employee to the database
-    const savedEmployee = await newEmployee.save();
+//     // Save the new employee to the database
+//     const savedEmployee = await newEmployee.save();
 
-    res.status(201).json(savedEmployee);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to add an employee" });
-  }
-};
+//     res.status(201).json(savedEmployee);
+//   } catch (error) {
+//     res.status(500).json({ error: "Failed to add an employee" });
+//   }
+// };
 
-// Controller to delete an employee by ID
-export const deleteEmployee = async (req, res) => {
-  try {
-    const employeeId = req.params.id; // Assuming you pass the employee ID in the URL
+// // Controller to delete an employee by ID
+// export const deleteEmployee = async (req, res) => {
+//   try {
+//     const employeeId = req.params.id; // Assuming you pass the employee ID in the URL
 
-    // Use the `findByIdAndDelete` method to delete the employee by ID
-    const deletedEmployee = await Employee.findByIdAndDelete(employeeId);
+//     // Use the `findByIdAndDelete` method to delete the employee by ID
+//     const deletedEmployee = await Employee.findByIdAndDelete(employeeId);
 
-    if (!deletedEmployee) {
-      return res.status(404).json({ error: "Employee not found" });
-    }
+//     if (!deletedEmployee) {
+//       return res.status(404).json({ error: "Employee not found" });
+//     }
 
-    res.status(200).json({ message: "Employee deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ error: "Failed to delete employee" });
-  }
-};
+//     res.status(200).json({ message: "Employee deleted successfully" });
+//   } catch (error) {
+//     res.status(500).json({ error: "Failed to delete employee" });
+//   }
+// };
 
-// Controller to update an employee by ID
-export const updateEmployee = async (req, res) => {
-  try {
-    const employeeId = req.params.id; // Assuming you pass the employee ID in the URL
-    const updateFields = req.body; // Fields to be updated
+// // Controller to update an employee by ID
+// export const updateEmployee = async (req, res) => {
+//   try {
+//     const employeeId = req.params.id; // Assuming you pass the employee ID in the URL
+//     const updateFields = req.body; // Fields to be updated
 
-    // Ensure at least one field is being updated
-    if (Object.keys(updateFields).length === 0) {
-      return res.status(400).json({ error: "No fields to update provided" });
-    }
+//     // Ensure at least one field is being updated
+//     if (Object.keys(updateFields).length === 0) {
+//       return res.status(400).json({ error: "No fields to update provided" });
+//     }
 
-    // Use the `findByIdAndUpdate` method to update the employee by ID
-    const updatedEmployee = await Employee.findByIdAndUpdate(
-      employeeId,
-      { $set: updateFields }, // Use $set to update only the provided fields
-      { new: true }
-    );
+//     // Use the `findByIdAndUpdate` method to update the employee by ID
+//     const updatedEmployee = await Employee.findByIdAndUpdate(
+//       employeeId,
+//       { $set: updateFields }, // Use $set to update only the provided fields
+//       { new: true }
+//     );
 
-    if (!updatedEmployee) {
-      return res.status(404).json({ error: "Employee not found" });
-    }
+//     if (!updatedEmployee) {
+//       return res.status(404).json({ error: "Employee not found" });
+//     }
 
-    res.status(200).json(updatedEmployee);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to update employee" });
-  }
-};
+//     res.status(200).json(updatedEmployee);
+//   } catch (error) {
+//     res.status(500).json({ error: "Failed to update employee" });
+//   }
+// };
 
-// Controller to get all employees with filter options and pagination
-export const getAllEmployees = async (req, res) => {
-    try {
-      const { page = 1, limit = 10, filter, search } = req.query;
-      const query = {};
+// // Controller to get all employees with filter options and pagination
+// export const getAllEmployees = async (req, res) => {
+//     try {
+//       const { page = 1, limit = 10, filter, search } = req.query;
+//       const query = {};
   
-      if (filter === "Waiter" || filter === "House_Keeper") {
-        query.designation = filter;
-      }
-      if (search) {
-        query.name = { $regex: search, $options: "i" };
-      }
+//       if (filter === "Waiter" || filter === "House_Keeper") {
+//         query.designation = filter;
+//       }
+//       if (search) {
+//         query.name = { $regex: search, $options: "i" };
+//       }
   
-      const options = {
-        page: parseInt(page, 10),
-        limit: parseInt(limit, 10),
-      };
-      const items = await Employee.paginate(query, options);
-      res.status(200).json(items);
-    } catch (error) {
-      res.status(500).json({ error: "Failed to retrieve items" });
-    }
-  };
+//       const options = {
+//         page: parseInt(page, 10),
+//         limit: parseInt(limit, 10),
+//       };
+//       const items = await Employee.paginate(query, options);
+//       res.status(200).json(items);
+//     } catch (error) {
+//       res.status(500).json({ error: "Failed to retrieve items" });
+//     }
+//   };
--- backend/models/Manager/employee.model.js
+++ backend/models/Manager/employee.model.js
@@ -1,51 +1,51 @@
-import mongoose from "mongoose";
-import mongoosePaginate from "mongoose-paginate-v2";
+// import mongoose from "mongoose";
+// import mongoosePaginate from "mongoose-paginate-v2";
 
-const employeeSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  designation: {
-    type: String,
-    required: false,
-    enum: ["Waiter", "House_Keeper"],
-  },
-  shift: {
-    type: String,
-    required: true,
-    enum: ["Day", "Night"],
-  },
-  sallary: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-  street_address: {
-    type: String,
-    required: false,
-  },
-  state: {
-    type: String,
-    required: false,
-  },
-  city: {
-    type: String,
-    required: false,
-  },
-  zip: {
-    type: String,
-    required: false,
-  },
-  images: {
-    type: Array,
-    required: false,
-  },
-});
+// const employeeSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//   },
+//   designation: {
+//     type: String,
+//     required: false,
+//     enum: ["Waiter", "House_Keeper"],
+//   },
+//   shift: {
+//     type: String,
+//     required: true,
+//     enum: ["Day", "Night"],
+//   },
+//   sallary: {
+//     type: Number,
+//     required: false,
+//     default: 0,
+//   },
+//   street_address: {
+//     type: String,
+//     required: false,
+//   },
+//   state: {
+//     type: String,
+//     required: false,
+//   },
+//   city: {
+//     type: String,
+//     required: false,
+//   },
+//   zip: {
+//     type: String,
+//     required: false,
+//   },
+//   images: {
+//     type: Array,
+//     required: false,
+//   },
+// });
 
-// Apply the mongoose-paginate-v2 plugin to your schema
-employeeSchema.plugin(mongoosePaginate);
+// // Apply the mongoose-paginate-v2 plugin to your schema
+// employeeSchema.plugin(mongoosePaginate);
 
-const Employee = mongoose.model("Employee", employeeSchema);
+// const Employee = mongoose.model("Employee", employeeSchema);
 
-export default Employee;
+// export default Employee;
