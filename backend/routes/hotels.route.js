e71360099ca3e6e20bf478d77ad16a46f6fc18e6
Dip Jit Baroi <dipjitbaroiofficial@gmail.com>
2023-11-07 13:36:22 +0600

    update: :+1:  removed unique hotel name and manager username for frontend requirement

--- backend/routes/hotels.route.js
+++ backend/routes/hotels.route.js
@@ -3,10 +3,11 @@
 
 // } from "../controllers/hotel.controller.js";
 import { checkToken } from "../middlewares/checkToken.js";
-import { addHotel } from "../controllers/hotel.controller.js";
+import { addHotel, getHotels } from "../controllers/hotel.controller.js";
 
 const router = Router();
 
 router.post("/add-hotel", checkToken, addHotel);
+router.get("/get-hotels", checkToken, getHotels);
 
 export default router;
