6eaa071ebde83568b00c3407b82ed9a4f438cd2c
jessanKhan <138293787+mahbub-dev-jsencoder@users.noreply.github.com>
2023-11-07 19:49:42 +0600

    owner list user updated by it's parents

--- frontend/src/redux/admin/ownerlist/ownerListAPI.js
+++ frontend/src/redux/admin/ownerlist/ownerListAPI.js
@@ -1,24 +1,35 @@
 import baseAPI from "../../baseAPI.js";
-
+const queryForAdmin = ({ cp, filter, search, role, parentId }) => {
+	if (parentId) {
+		return `users/get-users?page=${++cp}${
+			filter ? `&filter=${filter}` : ""
+		}${search ? `&search=${search}` : ""}${
+			role ? `&role=${role}` : ""
+		}&user_id=${parentId}`;
+	}else return `users/get-owners-by-admin?page=${++cp}${
+					filter ? `&filter=${filter}` : ""
+				}${search ? `&search=${search}` : ""}`
+}
 const ownerListAPI = baseAPI.injectEndpoints({
 	endpoints: (build) => ({
 		ownerList: build.query({
-			query: ({ cp, filter, search }) =>
-				`users/get-owners-by-admin?page=${++cp}${
-					filter ? `&filter=${filter}` : ""
-				}${search ? `&search=${search}` : ""}`,
+			query: queryForAdmin,
 			providesTags: ["owner"],
 		}),
 		getTransactionlogs: build.query({
 			query: ({ id, fromDate, toDate, cp }) =>
-				`/transactions/get-transactionlogs?user_id=${id}&fromDate=${fromDate||''}&toDate=${toDate||''}&page=${++cp}`,
+				`/transactions/get-transactionlogs?user_id=${id}&fromDate=${
+					fromDate || ""
+				}&toDate=${toDate || ""}&page=${++cp}`,
 			providesTags: ["transaction_log"],
 		}),
 		getStatuslogs: build.query({
-      query: ({ id, fromDate, toDate, cp }) => {
-        fromDate==='null' && (fromDate = '')
-        toDate==='null' && (toDate='')
-				return `/status/get-statuslogs?user_id=${id}&fromDate=${fromDate||''}&toDate=${toDate||''}&page=${++cp}`;
+			query: ({ id, fromDate, toDate, cp }) => {
+				fromDate === "null" && (fromDate = "");
+				toDate === "null" && (toDate = "");
+				return `/status/get-statuslogs?user_id=${id}&fromDate=${
+					fromDate || ""
+				}&toDate=${toDate || ""}&page=${++cp}`;
 			},
 			providesTags: ["status_log"],
 		}),
