3c26b1c6a52f0822621faea471999f7ab2fc1bef
Dip Jit Baroi <dipjitbaroiofficial@gmail.com>
2023-11-09 15:22:08 +0600

    feature: :lipstick: added functionalities for  getFoodsByHotelId

--- backend/controllers/Manager/food.controller.js
+++ backend/controllers/Manager/food.controller.js
@@ -1,171 +1,168 @@
 ﻿import Food from "../../models/Manager/food.model.js";
 
-
 export const addFood = async (req, res) => {
-	try {
-	  const { hotel_id, food_name, status, serveyor_quantity, price, images, description } = req.body;
-  
-	  const newFood = new Food({
-		hotel_id,
-		food_name,
-		status,
-		serveyor_quantity,
-		price,
-		images,
-		description,
-	  });
-  
-	  const savedFood = await newFood.save();
-  
-	  res.status(201).json({
-		success: true,
-		data: savedFood,
-		message: 'Food item added successfully',
-	  });
-	} catch (error) {
-	  res.status(500).json({
-		success: false,
-		error: 'Internal Server Error',
-	  });
-	}
-  };
+  try {
+    const {
+      hotel_id,
+      food_name,
+      status,
+      serveyor_quantity,
+      price,
+      images,
+      description,
+    } = req.body;
 
-export const getFood = async (req, res) => {
-	try {
-		const { page = 1, limit = 10, ...query } = req.query;
-		const parsedLimit = parseInt(limit);
-		const parsedPage = parseInt(page);
+    const newFood = new Food({
+      hotel_id,
+      food_name,
+      status,
+      serveyor_quantity,
+      price,
+      images,
+      description,
+    });
 
-		const startIndex = (parsedPage - 1) * parsedLimit;
-		const endIndex = parsedPage * parsedLimit;
+    const savedFood = await newFood.save();
 
-		const totalFoods = await Food.countDocuments(query);
-		const totalPages = Math.ceil(totalFoods / parsedLimit);
-		const pagination = {
-			total: totalFoods,
-			totalPages: totalPages,
-			currentPage: parsedPage,
-			limit: parsedLimit,
-		};
-		// search by food name
-		if (query.food_name) {
-			const food_name = new RegExp(query.food_name, "i");
-			const foods = await Food.find({ food_name })
-				.skip(startIndex)
-				.limit(parsedLimit);
-			res.status(200).json({
-				success: true,
-				data: foods,
-				pagination,
-				message: "Food items retrieved successfully",
-			});
+    res.status(201).json({
+      success: true,
+      data: savedFood,
+      message: "Food item added successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: "Internal Server Error",
+    });
+  }
+};
 
-			return;
-		}
+export const getFoodByHotelId = async (req, res) => {
+  try {
+    const { hotel_id } = req.params;
+    const { page = 1, limit = 10, filter, search } = req.query;
 
-		const foods = await Food.find(query)
-			.skip(startIndex)
-			.limit(parsedLimit);
-		res.status(200).json({
-			success: true,
-			data: foods,
-			pagination,
-			message: "Food items retrieved successfully",
-		});
-	} catch (error) {
-		res.status(500).json({
-			success: false,
-			error: "Internal Server Error",
-		});
-	}
+    const query = {
+      hotel_id,
+    };
+
+    if (["Available", "Unavailable"].includes(filter)) {
+      query.status = filter;
+    }
+
+    if (search) {
+      query.$or = [
+        { food_name: { $regex: search, $options: "i" } },
+        { description: { $regex: search, $options: "i" } },
+      ];
+    }
+
+    const options = {
+      page: parseInt(page, 10),
+      limit: parseInt(limit, 10),
+    };
+
+    const foods = await Food.paginate(query, options);
+
+    res.status(200).json({
+      success: true,
+      data: foods,
+      message: "Rooms retrieved successfully",
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      error: error,
+      message: "Internal Server Error",
+    });
+  }
 };
 
 export const getfoodById = async (req, res) => {
-	try {
-		const food = await Food.findById(req.params.foodId);
-		res.status(200).json({
-			success: true,
-			data: food,
-			message: "Food item retrieved successfully",
-		});
-	} catch (error) {
-		res.status(500).json({
-			success: false,
-			error: "Internal Server Error",
-		});
-	}
+  try {
+    const food = await Food.findById(req.params.foodId);
+    res.status(200).json({
+      success: true,
+      data: food,
+      message: "Food item retrieved successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: "Internal Server Error",
+    });
+  }
 };
 
 export const updatefood = async (req, res) => {
-	try {
-		const updatedFood = await Food.findByIdAndUpdate(
-			req.params.foodId,
-			req.body,
-			{ new: true }
-		);
+  try {
+    const updatedFood = await Food.findByIdAndUpdate(
+      req.params.foodId,
+      req.body,
+      { new: true }
+    );
 
-		res.status(200).json({
-			success: true,
-			data: updatedFood,
-			message: "Food item updated successfully",
-		});
-	} catch (error) {
-		res.status(500).json({
-			success: false,
-			error: "Internal Server Error",
-		});
-	}
+    res.status(200).json({
+      success: true,
+      data: updatedFood,
+      message: "Food item updated successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: "Internal Server Error",
+    });
+  }
 };
 
 export const deletefood = async (req, res) => {
-	try {
-		const deletedFood = await Food.findByIdAndDelete(req.params.foodId);
+  try {
+    const deletedFood = await Food.findByIdAndDelete(req.params.foodId);
 
-		if (!deletedFood) {
-			return res.status(404).json({
-				success: false,
-				error: "Food item not found",
-			});
-		}
+    if (!deletedFood) {
+      return res.status(404).json({
+        success: false,
+        error: "Food item not found",
+      });
+    }
 
-		res.status(200).json({
-			success: true,
-			data: {},
-			message: "Food item deleted successfully",
-		});
-	} catch (error) {
-		res.status(500).json({
-			success: false,
-			error: "Internal Server Error",
-		});
-	}
+    res.status(200).json({
+      success: true,
+      data: {},
+      message: "Food item deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: "Internal Server Error",
+    });
+  }
 };
 
 // order
 export const addOrder = async (req, res) => {
-	try {
-		const {foods,room} = req.body;
+  try {
+    const { foods, room } = req.body;
 
-    
-
-
-		const orderFoods = foods?.map(async (element) => {
-			const {food, quantity, } = element;
-			let foodItem = await Food.findById(food);
-			foodItem = { sell: foodItem.sell + 1, ...foodItem };
-			await foodItem.save();
-			// await Food.findByIdAndUpdate(food, { $inc: { sell: 1 } });
+    const orderFoods = foods?.map(async (element) => {
+      const { food, quantity } = element;
+      let foodItem = await Food.findById(food);
+      foodItem = { sell: foodItem.sell + 1, ...foodItem };
+      await foodItem.save();
+      // await Food.findByIdAndUpdate(food, { $inc: { sell: 1 } });
     });
     const order = new FoodOrder({
       room,
-      foods:orderFoods
+      foods: orderFoods,
     });
     await order.save();
-		res.status(201).json({ message: "Order created successfully" });
-	} catch (error) {
-		res.status(500).json({
-			message: "Error creating order",
-			error: error.message,
-		});
-	}
+    res.status(201).json({ message: "Order created successfully" });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error creating order",
+      error: error.message,
+    });
+  }
 };
--- backend/routes/Manager/food.route.js
+++ backend/routes/Manager/food.route.js
@@ -1,17 +1,24 @@
 ﻿import { Router } from "express";
 import {
-    addFood, getFood, getfoodById, updatefood,deletefood,addOrder
+  addFood,
+//   getFood,
+  getfoodById,
+  updatefood,
+  deletefood,
+  addOrder,
+  getFoodByHotelId,
 } from "../../controllers/Manager/food.controller.js";
 import { checkToken } from "../../middlewares/checkToken.js";
 
 const router = Router();
 
-// add room 
-router.post('/add-food', checkToken, addFood);
-router.get('/get-food', checkToken, getFood);
-router.get('/get-food-by-id/:foodId', checkToken, getfoodById);
-router.patch('/update-food/:foodId', checkToken, updatefood);
-router.delete('/delete-food/:foodId', checkToken, deletefood);
-router.post('/add-order',checkToken,addOrder)
+// add room
+router.post("/add-food", checkToken, addFood);
+// router.get("/get-food", checkToken, getFood);
+router.get("/get-foods-by-hotel/:hotel_id", checkToken, getFoodByHotelId);
+router.get("/get-food-by-id/:foodId", checkToken, getfoodById);
+router.patch("/update-food/:foodId", checkToken, updatefood);
+router.delete("/delete-food/:foodId", checkToken, deletefood);
+router.post("/add-order", checkToken, addOrder);
 
 export default router;
