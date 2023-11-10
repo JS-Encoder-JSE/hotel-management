a9f4e08593092e99b5b6830eec0f63dbda074b6a
Dip Jit Baroi <dipjitbaroiofficial@gmail.com>
2023-11-10 17:34:50 +0600

    update: :+1:  populated room_ids field

--- backend/models/Manager/booking.model.js
+++ backend/models/Manager/booking.model.js
@@ -1,104 +1,109 @@
 ﻿import mongoose from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
+import Room from "./room.model.js";
 const ImageSchema = new mongoose.Schema({
   driving_lic_img: { type: Array, required: false, default: "" },
   passport: { type: Array, required: false, default: "" },
   nid: { type: Array, required: false, default: "" },
 });
-const bookingSchema = new mongoose.Schema({
-  room_ids: [
-    {
+const bookingSchema = new mongoose.Schema(
+  {
+    room_ids: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Room",
+      },
+    ],
+    hotel_id: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
     },
-  ],
-  hotel_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
+    guestName: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: false,
+    },
+    mobileNumber: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    emergency_contact: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    adult: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    children: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    paymentMethod: {
+      type: String,
+      enum: ["Card", "Cash", "Mobile_Banking"],
+      required: true,
+    },
+    transection_id: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    amount: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    total_amount: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    paid_amount: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    total_unpaid_amount: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    discount: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    from: { type: Date, required: true },
+    to: { type: Date, required: true },
+    status: {
+      type: String,
+      enum: ["Active", "CheckedIn", "CheckedOut", "Canceled"],
+      required: true,
+      default: "Active",
+    },
+    nationality: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    doc_number: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    doc_images: ImageSchema,
   },
-  guestName: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: false,
-  },
-  mobileNumber: {
-    type: String,
-    required: false,
-    default: "",
-  },
-  emergency_contact: {
-    type: String,
-    required: false,
-    default: "",
-  },
-  adult: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-  children: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-  paymentMethod: {
-    type: String,
-    enum: ["Card", "Cash", "Mobile_Banking"],
-    required: true,
-  },
-  transection_id: {
-    type: String,
-    required: false,
-    default: "",
-  },
-  amount: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-  total_amount: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-  paid_amount: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-  total_unpaid_amount: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-  discount: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-  from: { type: Date, required: true },
-  to: { type: Date, required: true },
-  status: {
-    type: String,
-    enum: ["Active", "CheckedIn", "CheckedOut", "Canceled"],
-    required: true,
-    default: "Active",
-  },
-  nationality: {
-    type: String,
-    required: false,
-    default: "",
-  },
-  doc_number: {
-    type: String,
-    required: false,
-    default: "",
-  },
-  doc_images: ImageSchema,
-},{ timeStamp: true });
+  { timeStamp: true }
+);
 // Apply the mongoose-paginate-v2 plugin to your schema
 bookingSchema.plugin(mongoosePaginate);
 const Booking = mongoose.model("Booking", bookingSchema);
