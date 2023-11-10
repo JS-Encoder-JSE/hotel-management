64bd0be345c1a4363de966e942d107b66fee46a8
Dip Jit Baroi <dipjitbaroiofficial@gmail.com>
2023-11-10 18:34:24 +0600

    feature: :lipstick:  added multiple apis for gym

---
+++ backend/controllers/Manager/gym.controller.js
@@ -0,0 +1,173 @@
+import mongoose from "mongoose";
+import GymBills from "../../models/Manager/gym.model.js";
+import Room from "../../models/Manager/room.model.js";
+
+export const addGymBill = async (req, res) => {
+  try {
+    const {
+      name,
+      hotel_id,
+      room_id,
+      members,
+      price,
+      paid_amount = 0,
+    } = req.body;
+
+    const unpaid_amount = Math.max(price - paid_amount, 0);
+    let status = "Pending";
+    if (paid_amount >= price) {
+      status = "Paid";
+    } else if (paid_amount > 0) {
+      status = "Partial";
+    }
+
+    const newGymBill = new GymBills({
+      name,
+      hotel_id: hotel_id,
+      room_id: room_id,
+      members,
+      price,
+      paid_amount,
+      unpaid_amount,
+      status,
+    });
+
+    const savedGymBill = await newGymBill.save();
+
+    res.status(201).json({
+      success: true,
+      data: savedGymBill,
+      message: "Gym bill added successfully",
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      error: "Internal Server Error",
+    });
+  }
+};
+
+export const getGymBillsByHotelId = async (req, res) => {
+  try {
+    const { hotel_id } = req.params;
+    const { page = 1, limit = 10, search, filter } = req.query;
+
+    const query = { hotel_id };
+
+    if (search) {
+      const room = await Room.findOne({ hotel_id, roomNumber: search });
+      if (room) {
+        query.room_id = room._id;
+      } else {
+        return res.status(200).json({
+          success: true,
+          data: [],
+          message: "No Gym Bills found for the given roomNumber",
+        });
+      }
+    }
+
+    if (["Pending", "Paid", "Partial"].includes(filter)) {
+      query.status = filter;
+    }
+
+    const options = {
+      page: parseInt(page, 10),
+      limit: parseInt(limit, 10),
+    };
+
+    const gymBills = await GymBills.paginate(query, options);
+
+    res.status(200).json({
+      success: true,
+      data: gymBills,
+      message: "Gym Bills retrieved successfully",
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      error: "Internal Server Error",
+    });
+  }
+};
+
+export const getGymBillById = async (req, res) => {
+  try {
+    const gymBillId = req.params.bill_id;
+
+    if (!mongoose.Types.ObjectId.isValid(gymBillId)) {
+      return res.status(400).json({
+        success: false,
+        error: "Invalid gym bill ID",
+      });
+    }
+
+    const gymBill = await GymBills.findById(gymBillId);
+
+    if (!gymBill) {
+      return res.status(404).json({
+        success: false,
+        error: "Gym bill not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: gymBill,
+      message: "Gym bill retrieved successfully",
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      error: "Internal Server Error",
+    });
+  }
+};
+
+export const updateGymBill = async (req, res) => {
+  try {
+    const gymBillId = req.params.bill_id;
+    const updateData = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(gymBillId)) {
+      return res.status(400).json({
+        success: false,
+        error: "Invalid gym bill ID",
+      });
+    }
+
+    const existingGymBill = await GymBills.findById(gymBillId);
+
+    if (!existingGymBill) {
+      return res.status(404).json({
+        success: false,
+        error: "Gym bill not found",
+      });
+    }
+
+    const updatedGymBill = await GymBills.findByIdAndUpdate(
+      gymBillId,
+      updateData,
+      { new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      data: updatedGymBill,
+      message: "Gym bill updated successfully",
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      error: "Internal Server Error",
+    });
+  }
+};
---
+++ backend/models/Manager/gym.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+const gymBillSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    hotel_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: false,
+    },
+    room_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: false,
+    },
+    members: {
+      type: String,
+      required: false,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    paid_amount: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    unpaid_amount: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["Pending", "Paid", "Partial"],
+      default: "Pending",
+    },
+  },
+  { timeStamp: true }
+);
+
+// Apply the mongoose-paginate-v2 plugin to your schema
+gymBillSchema.plugin(mongoosePaginate);
+
+const GymBills = mongoose.model("GymBills", gymBillSchema);
+
+export default GymBills;
