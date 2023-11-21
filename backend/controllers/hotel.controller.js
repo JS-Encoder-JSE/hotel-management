86935a32c2f9f4b8185798d8036bd8c6d4872854
Dip Jit Baroi <dipjitbaroiofficial@gmail.com>
2023-11-21 14:09:05 +0600

    update: :+1:  minor changes in getHotelByManagerId

--- backend/controllers/hotel.controller.js
+++ backend/controllers/hotel.controller.js
@@ -235,9 +235,7 @@ export const getHotelsByManagerId = async (req, res) => {
     const { manager_id } = req.params; // Assuming managerId is in the URL parameters
 
     // Use Mongoose to find hotels that have a manager with the specified ID
-    const hotels = await Hotel.find({ "managers._id": manager_id }).select(
-      "-managers"
-    );
+    const hotels = await Hotel.find({ manager_acc: manager_id });
 
     if (!hotels || hotels.length === 0) {
       return res
