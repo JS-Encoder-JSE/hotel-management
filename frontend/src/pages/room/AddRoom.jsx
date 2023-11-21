31ee01afbb0f2afd17f9db468e47941bd6e75b79
Md.Abu siddik <dev.antor@jsencoder.com>
2023-11-21 15:05:51 +0600

    add room update

--- frontend/src/pages/room/AddRoom.jsx
+++ frontend/src/pages/room/AddRoom.jsx
@@ -329,7 +329,7 @@ const AddRoom = () => {
                   </option>
                   <option value={1}>1</option>
                   <option value={2}>2</option>
-                  <option value={4}>4</option>
+                  <option value={3}>3</option>
                 </select>
                 {formik.touched.capacity && Boolean(formik.errors.capacity) ? (
                   <small className="text-red-600">
