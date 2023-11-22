2e277303995af0b8b7aa1629e1c1bef216f18768
Md.Abu siddik <dev.antor@jsencoder.com>
2023-11-22 18:36:29 +0600

    edit room update

--- frontend/src/pages/room/EditRoom.jsx
+++ frontend/src/pages/room/EditRoom.jsx
@@ -297,6 +297,7 @@ const EditRoom = () => {
               </option>
               <option value={1}>1</option>
               <option value={2}>2</option>
+              <option value={3}>3</option>
               <option value={4}>4</option>
             </select>
             {formik.touched.capacity && Boolean(formik.errors.capacity) ? (
