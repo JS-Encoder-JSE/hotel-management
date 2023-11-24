24cc2a1080c88f23c5a3ac38b6c283d36d969680
Dip Jit Baroi <dipjitbaroiofficial@gmail.com>
2023-11-24 20:48:31 +0600

    feature: :lipstick:  added getExpenseById

--- backend/controllers/expense.controller.js
+++ backend/controllers/expense.controller.js
@@ -99,3 +99,21 @@ export const getExpenses = async (req, res) => {
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+export const getExpenseById = async (req, res) => {
+    try {
+      const { expense_id } = req.params;
+  
+      // Find the expense by ID
+      const expense = await Expense.findById(expense_id);
+  
+      if (!expense) {
+        return res.status(404).json({ error: 'Expense not found' });
+      }
+  
+      return res.status(200).json(expense);
+    } catch (error) {
+      console.error('Error fetching expense by ID:', error);
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+  };
