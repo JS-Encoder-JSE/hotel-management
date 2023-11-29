d1600529b5a3586314395da3820eb38980263ce2
Dip Jit Baroi <dipjitbaroiofficial@gmail.com>
2023-11-29 21:53:33 +0600

    update: :+1:  feature added getBookingDetailsById

--- backend/routes/Manager/booking.route.js
+++ backend/routes/Manager/booking.route.js
@@ -7,6 +7,7 @@ import {
   deleteBooking,
   getActiveBookingByRoomId,
   cancelBooking,
+  getBookingDetailsById,
 } from "../../controllers/Manager/booking.controller.js";
 import { checkToken } from "../../middlewares/checkToken.js";
 import {
@@ -21,6 +22,11 @@ router.post("/add-booking", checkToken, addBooking);
 router.get("/get-bookings-by-hotel", checkToken, getBookingsByHotel);
 router.get("/get-booking-by-id/:booking_id", checkToken, getBookingById);
 router.get("/get-active-booking-by-room/:room_id", getActiveBookingByRoomId);
+router.get(
+  "/get-booking-details/:booking_id",
+  checkToken,
+  getBookingDetailsById
+);
 router.patch("/update-booking/:booking_id", checkToken, updateBooking);
 router.get(
   "/get-checkoutinfo-by-room/:room_id",
@@ -28,7 +34,7 @@ router.get(
   getCheckoutInfoByRoom
 );
 router.post("/checkout", checkToken, checkedOut);
-router.delete("/cancel-booking/:booking_id", cancelBooking);
+router.delete("/cancel-booking/:booking_id", checkToken, cancelBooking);
 // router.delete('/delete-booking/:booking_id', checkToken, deleteBooking);
 
 export default router;
