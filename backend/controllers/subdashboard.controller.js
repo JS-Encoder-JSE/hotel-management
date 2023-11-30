6f56626f78295555a8d038b52d992357395dabff
Dip Jit Baroi <dipjitbaroiofficial@gmail.com>
2023-11-30 08:54:29 +0600

    feature: :lipstick:  added getSubdashboardInfo

--- backend/controllers/subdashboard.controller.js
+++ backend/controllers/subdashboard.controller.js
@@ -1,4 +1,8 @@
-import { StaticSubDashData } from "../models/subdashboard.model.js";
+import {
+  DailySubDashData,
+  MonthlySubDashData,
+  StaticSubDashData,
+} from "../models/subdashboard.model.js";
 
 export const addStaticSubDashData = async (req, res) => {
   try {
@@ -21,5 +25,44 @@ export const addStaticSubDashData = async (req, res) => {
   } catch (error) {
     console.error("Error adding static sub-dashboard data:", error);
     return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const getSubDashboardInfo = async (req, res) => {
+  try {
+    const userId = req.params.user_id;
+    const currentDate = new Date();
+    currentDate.setHours(0, 0, 0, 0);
+    const date = currentDate.toISOString();
+
+    const permanent_datas = await StaticSubDashData.findOne({
+      user_id: userId,
+    });
+
+    if (!permanent_datas) {
+      return res.status(404).json({ message: "permanent_datas not found" });
+    }
+    // Calculate the start date for fetching the last 12 months
+    const startDate = new Date();
+    startDate.setMonth(startDate.getMonth() - 11);
+    const monthly_datas = await MonthlySubDashData.find({
+      user_id: userId,
+      createdAt: { $gte: startDate, $lte: currentDate },
+    });
+    const daily_datas = await DailySubDashData.find({
+      user_id: userId,
+      date,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Succesfully fetched subdashboard informations",
+      daily_datas: daily_datas,
+      permanent_datas: permanent_datas,
+      monthly_datas: monthly_datas,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to get dashboard" });
   }
 };
