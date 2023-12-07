f789adff2644d656c343f1c2ee78f63a6cf0c117
Abul Basar <129590292+devBasar@users.noreply.github.com>
2023-12-08 01:37:00 +0600

    update modal

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -4,7 +4,7 @@ const Modal = ({ id, children, classNames }) => {
   return (
     <dialog id={id} className="modal">
       <div
-        className={`modal-box scrollbar-none ${classNames ? classNames : ""}`}
+        className={`modal-box  scrollbar-none ${classNames ? classNames : ""}`}
       >
         {children}
       </div>
