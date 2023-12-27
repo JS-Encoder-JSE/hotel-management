b0e30c5d1699eeae917b7f064d228cfc02a0814e
Md.Abu siddik <dev.antor@jsencoder.com>
2023-12-27 13:26:06 +0600

    remove the comment

--- frontend/src/components/room/EditBooking.jsx
+++ frontend/src/components/room/EditBooking.jsx
@@ -57,7 +57,7 @@ const validationSchema = yup.object({
 
 const EditBooking = ({ data, bookingId }) => {
   const { id } = useParams();
- 
+
   // current date for from
   const [currentDate, setCurrentDate] = useState(new Date());
   const [updateBookingInfo, { isLoading }] = useUpdateBookingInfoMutation();
@@ -80,45 +80,6 @@ const EditBooking = ({ data, bookingId }) => {
     },
 
     validationSchema,
-    // onSubmit: async (values, formikHelpers) => {
-    //   const obj = {
-    //     guestName: values.guestName,
-    //     address: values.address,
-    //     mobileNumber: values.mobileNumber,
-    //     emergency_contact: values.emergency_contact,
-    //     adult: Number(values.adult),
-    //     children: Number(values.children),
-    //     nationality: values.nationality,
-    //   };
-     
-    //   try {
-    //     const response = await updateBookingInfo({
-    //       id: id,
-    //       data: obj,
-    //     });
-
-    //     if (response?.error) {
-    //       toast.error(response.error.data.message);
-    //     } else {
-    //       // formikHelpers.resetForm();
-    //       // closeRef.current.click();
-    //       // toast.success(response.data.message);
-    //     }
-    //     const responses = await updateBooking({
-    //       id: id,
-    //       data : updateBookings
-    //     });
-    //     if (response?.error) {
-    //       toast.error(response.error.data.message);
-    //     }
-    //     else {
-    //       formikHelpers.resetForm();
-    //       closeRef.current.click();
-    //       toast.success(response.data.message);
-    //     }
-    //   } 
-    //   catch (error) {}
-    // },
     onSubmit: async (values, formikHelpers) => {
       const obj = {
         guestName: values.guestName,
@@ -129,14 +90,14 @@ const EditBooking = ({ data, bookingId }) => {
         children: Number(values.children),
         nationality: values.nationality,
       };
-    
+
       try {
         // First asynchronous operation
         const response = await updateBookingInfo({
           id: id,
           data: obj,
         });
-    
+
         if (response?.error) {
           toast.error(response.error.data.message);
         } else {
@@ -145,7 +106,7 @@ const EditBooking = ({ data, bookingId }) => {
             id: id,
             data: obj,
           });
-    
+
           if (updateBookingResponse?.error) {
             toast.error(updateBookingResponse.error.data.message);
           } else {
@@ -156,10 +117,9 @@ const EditBooking = ({ data, bookingId }) => {
         }
       } catch (error) {
         // Handle errors for both asynchronous operations
-        console.error('Error:', error);
+        console.error("Error:", error);
       }
     },
-    
   });
 
   useEffect(() => {
